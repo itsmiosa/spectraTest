9884a8d835421a3b138704722f1b1f3a2f79479f
Miguel Rosa <73667613+itsmiosa@users.noreply.github.com>
2025-06-30 02:23:27 +0100

    Update spectraTest.py

--- spectraTest.py
+++ spectraTest.py
@@ -1,10 +1,12 @@
 from spectral import envi
+from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
+
 # Choose pixel to visualize
-x = 300
+x = 600
 y = 150
 
 # Load metadata and image
@@ -17,6 +19,16 @@ spectrum = np.array(img[x, y])
 wavelengths = img.metadata.get("wavelength")
 wavelengths = np.array([float(w) for w in wavelengths]) if wavelengths else np.arange(len(spectrum))
 
+# Export full spectrum to CSV
+timestamp = datetime.now().strftime(r"[%Y-%m-%d]_[%Hh%Mm%Ss]")
+output_filename = f"spectrum_({x}x {y})_{timestamp}.csv"
+with open(output_filename, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Wavelength (nm)', 'Reflectance'])
+    for wl, ref in zip(wavelengths, spectrum):
+        writer.writerow([wl, ref])
+print(f"Saved full spectrum to: {output_filename}")
+
 # Plot full spectrum
 plt.figure(figsize=(10, 5))
 plt.plot(wavelengths, spectrum)
@@ -27,15 +39,6 @@ plt.grid(True)
 plt.tight_layout()
 plt.show()
 
-# Export full spectrum to CSV
-output_filename = f"spectrum_{x}_{y}_full.csv"
-with open(output_filename, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['Wavelength (nm)', 'Reflectance'])
-    for wl, ref in zip(wavelengths, spectrum):
-        writer.writerow([wl, ref])
-
-print(f"Saved full spectrum to: {output_filename}")
 
 
-#test
+
